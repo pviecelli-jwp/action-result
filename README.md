3743960f9f05f477ba5f4235e605e01182a03dee
Pietro Viecelli <pviecelli@jwplayer.com>
2022-06-13 15:36:36 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # action-result
-Dotnet library that implements a similar concept to Rust Result
+Dotnet library that implements a similar concept to Rust's [Result](https://doc.rust-lang.org/std/result/)
---
+++ src/ActionResult/ActionResult.cs
@@ -0,0 +1,33 @@
+﻿namespace ActionResult
+{
+    public class ActionResult<T, E>
+    {
+        internal T? Value { get; set; }
+        internal E? Error { get; set; }
+        public bool IsSuccess { get; set; }
+
+        internal ActionResult(E error)
+        {
+            Value = default;
+            IsSuccess = false;
+            Error = error;
+        }
+
+        internal ActionResult(T value)
+        {
+            Value = value;
+            IsSuccess = true;
+            Error = default;
+        }
+
+        public static ActionResult<T, E> Success(T value)
+        {
+            return new ActionResult<T, E>(value);
+        }
+
+        public static ActionResult<T, E> Failure(E result)
+        {
+            return new ActionResult<T, E>(result);
+        }
+    }
+}
